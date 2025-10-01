211bf362e279da0fa67aef037bd08003e3530871
shiwwamm <vermashivams1510@gmail.com>
2025-10-01 00:55:44 +0000

    Clean up

--- topologies/simple_topo.py
+++ topologies/simple_topo.py
@@ -25,12 +25,17 @@ def myNetwork():
     h3 = net.addHost('h3', ip='10.0.0.3/24')
     h4 = net.addHost('h4', ip='10.0.0.4/24')
     h5 = net.addHost('h5', ip='10.0.0.5/24')
+    h6 = net.addHost('h6', ip='10.0.0.6/24')
+    h7 = net.addHost('h7', ip='10.0.0.7/24')
 
     # Add one switch
     s1 = net.addSwitch('s1')
     s2 = net.addSwitch('s2')
     s3 = net.addSwitch('s3')
     s4 = net.addSwitch('s4')
+    s5 = net.addSwitch('s5')
+    s6 = net.addSwitch('s6')
+    s7 = net.addSwitch('s7')
 
     info('*** Creating links\n')
     # Link the hosts to the switch
@@ -39,10 +44,15 @@ def myNetwork():
     net.addLink(h3, s3)
     net.addLink(h4, s4)
     net.addLink(h5, s2)
+    net.addLink(h6, s5)
+    net.addLink(h7, s6)
     
     net.addLink(s1, s2)
     net.addLink(s2, s3)
     net.addLink(s3, s4)
+    net.addLink(s4, s5)
+    net.addLink(s5, s6)
+    net.addLink(s3, s7)
 
     info('*** Starting network\n')
     net.start()
